92b55b7311e9b185739bb6ec1029cf8edc3efff8
Levente <suvegjartolevente@gmail.com>
2024-06-23 20:12:48 +0200

    Health Labels bg color added

--- src/components/ui/HealthLabels.jsx
+++ src/components/ui/HealthLabels.jsx
@@ -1,21 +1,18 @@
-import {
-    Box,
-    Text,
-    
-  } from "@chakra-ui/react";
-  
-  export const HealthLabelListing = ({ healthlabels }) => {
-    return (
-      <Box mb={4}>
-        <Text fontSize="lg" fontWeight="bold" mb={2}>
-          Health labels:
-        </Text>
-        {healthlabels.map((healthlabels, index) => (
-          <Text key={index} fontSize="md">
-            {healthlabels}
+import { Box, Text } from "@chakra-ui/react";
+
+export const HealthLabelListing = ({ healthlabels }) => {
+  return (
+    <Box mb={4}>
+      <Text fontSize="lg" fontWeight="bold" mb={2}>
+        Health labels:
+      </Text>
+      {healthlabels.map((label, index) => (
+        <Box key={index} bg="#c974e8" p={2} mb={2} borderRadius="md">
+          <Text fontSize="md" color="white">
+            {label}
           </Text>
-        ))}
-      </Box>
-    );
-  };
-  
+        </Box>
+      ))}
+    </Box>
+  );
+};
