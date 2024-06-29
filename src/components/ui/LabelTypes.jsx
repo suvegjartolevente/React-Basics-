26611d5beca6f3cf6c9f11cab46f58e8db8f6dee
Levente <suvegjartolevente@gmail.com>
2024-06-29 10:50:09 +0200

    Item card ready

--- src/components/ui/LabelTypes.jsx
+++ src/components/ui/LabelTypes.jsx
@@ -1,13 +1,28 @@
 import { Box, Text } from "@chakra-ui/react";
 
-export const LabelListing = ({ listLabels, title, bgColor, fontWeight = "bold" }) => {
+export const LabelListing = ({
+  listLabels,
+  title,
+  bgColor,
+  fontWeight = "bold",
+  alignItems = "center", 
+  textAlign = "center", 
+}) => {
   return (
-    <Box mb={4}>
-      <Text fontSize="lg"  mb={2} color="black"  fontWeight={fontWeight}> 
+    <Box mb={4} display="flex" flexDirection="column" alignItems={alignItems}>
+      <Text fontSize="lg" mb={2} color="black" fontWeight={fontWeight}>
         {title}
       </Text>
       {listLabels.map((label, index) => (
-        <Box key={index} bg={bgColor} p={1} mb={1} borderRadius="md" width={"12em"}>
+        <Box
+          key={index}
+          bg={bgColor}
+          p={1}
+          mb={1}
+          borderRadius="md"
+          width={"12em"}
+          textAlign={textAlign} 
+        >
           <Text fontSize="sm" color="white">
             {label}
           </Text>
