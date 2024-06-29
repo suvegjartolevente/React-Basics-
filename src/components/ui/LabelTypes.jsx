50028443360d3b755240b0fd1e40e86c7eccda85
Levente <suvegjartolevente@gmail.com>
2024-06-29 10:36:16 +0200

    Before screw up

--- src/components/ui/LabelTypes.jsx
+++ src/components/ui/LabelTypes.jsx
@@ -1,9 +1,9 @@
 import { Box, Text } from "@chakra-ui/react";
 
-export const LabelListing = ({ listLabels, title, bgColor }) => {
+export const LabelListing = ({ listLabels, title, bgColor, fontWeight = "bold" }) => {
   return (
     <Box mb={4}>
-      <Text fontSize="lg" fontWeight="bold" mb={2}>
+      <Text fontSize="lg"  mb={2} color="black"  fontWeight={fontWeight}> 
         {title}
       </Text>
       {listLabels.map((label, index) => (
--- src/components/ui/RecipeItemCard.jsx
+++ src/components/ui/RecipeItemCard.jsx
@@ -1,6 +1,20 @@
 import { Box, Image, Text } from "@chakra-ui/react";
+import { LabelListing } from "./LabelTypes";
+
+
 
 export const RecipeItemCard = ({ recipe, onClick }) => {
+  const {
+    dietLabels,
+    cautions,
+     dishType,
+     healthLabels
+  } = recipe;
+
+  const filteredHealthLabels = healthLabels?.filter(
+    (label) => label === "Vegan" || label === "Vegetarian"
+  );
+
   return (
     <Box
       onClick={onClick}
@@ -17,23 +31,52 @@ export const RecipeItemCard = ({ recipe, onClick }) => {
       boxShadow="md"
       _hover={{ transform: "scale(1.01)" }}
     >
-      <Box bg="white" borderRadius="lg">
-        <Box w="100%" p={2}>
+      <Box bg="#e8e1d5" borderRadius="lg" width="100%" height="100%" display="flex" flexDirection="column" >
+        <Box w="100%" p={2} height="250px" overflow="hidden" > 
           <Image
             src={recipe.image}
-            w="100%"
-            h="200px"
-            objectFit="cover"
+             w="100%"
+             h="100%"
+             objectFit="cover"
             borderRadius="md"
           />
         </Box>
-        <Box textAlign="center" mt={4}>
-          <Text as="h1" fontWeight="bold" color="black" p={4}>
-            {recipe.label}
-          </Text>
+        <Box textAlign="center"   mt={2} mx="auto">
           <Text as="h2" color="black" p={2}>
             {recipe.mealType}
           </Text>
+          <Text as="h1" fontWeight="bold" color="black" p={4}>
+            {recipe.label}
+          </Text>
+          {filteredHealthLabels && filteredHealthLabels.length > 0 && (
+            <LabelListing
+              listLabels={filteredHealthLabels}
+              title="Health labels:"
+              bgColor="#c974e8"
+              fontWeight="bold"
+            />
+          )}
+          {dietLabels && dietLabels.length > 0 && (
+            <LabelListing
+              listLabels={dietLabels}
+              fontWeight="normal"
+              bgColor="#52e385"
+            />
+          )}
+          <Text fontSize="md" mb={4} color="black">
+          Dish: {recipe.dishType}
+            </Text>
+          {cautions && cautions.length > 0 && (
+            
+            <LabelListing
+              listLabels={cautions}
+              fontWeight="normal"
+              title="Cautions:" 
+              color= "black"
+              bgColor="#e89b2e"
+            />
+            
+          )}
         </Box>
       </Box>
     </Box>
